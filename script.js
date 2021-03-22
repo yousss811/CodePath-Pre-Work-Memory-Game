acf49fc2d2c352106d7c19ee65046a55ab3cbacb
Glitch (hello-webpage) <none>
2021-03-22 06:16:21 +0000

    🏆🎒 Checkpoint ./script.js:30019375/386 ./index.html:30019375/310

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
-
 var pattern = [2,2,4,3,2,1,2,4]; 
 var progress = 0; 
 var gamePlaying = false; 
 
 function startGame(){
+  console.log("startgame");
   progress = 0;
   gamePlaying = true; 
   
   document.getElementById("startBtn").classList.add("hidden"); 
-  document.getElementById("stopBtn")
+  document.getElementById("stopBtn").classList.remove("hidden"); 
 }
 
 function stopGame(){
+  console.log("stopgame");
   gamePlaying = false; 
+  
+  document.getElementById("startBtn").classList.remove("hidden"); 
+  document.getElementById("stopBtn").classList.add("hidden"); 
 }
