1dbe0129ca38e8bc29502bfbd6458e431b3eaa19
Glitch (hello-webpage) <none>
2021-03-22 07:27:26 +0000

    📠🍅 Checkpoint ./style.css:30019375/29 ./index.html:30019375/262 ./script.js:30019375/1435

--- script.js
+++ script.js
@@ -3,20 +3,64 @@
 
 var pattern = [2,2,4,3,2,1,2,4]; 
 var progress = 0; 
-var gamePlaying = false; 
+var gamePlaying = false;
+
+var tonePlaying = false;
+var volume = .5; 
 
 function startGame(){
   progress = 0;
   gamePlaying = true; 
     
-  document.getElementById("startBtn").style.visibility="hidden"; 
-  document.getElementById("stopBtn").style.visibility="visible"; 
+  document.getElementById("startBtn").classList.add("hidden");
+document.getElementById("stopBtn").classList.remove("hidden");
 }
 
 function stopGame(){
   console.log("stopgame");
   gamePlaying = false;
   
-  document.getElementById("startBtn").style.visibility="visible"; 
-  document.getElementById("stopBtn").style.visibility="hidden"; 
+  document.getElementById("stopBtn").classList.add("hidden");
+document.getElementById("startBtn").classList.remove("hidden");
 }
+
+
+
+
+
+// Sound Synthesis Functions
+const freqMap = {
+  1: 261.6,
+  2: 329.6,
+  3: 392,
+  4: 446.2
+}
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    tonePlaying = true
+  }
+}
+function stopTone(){
+    g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+    tonePlaying = false
+}
+
+//Page Initialization
+// Init Sound Synthesizer
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
