4adb085ddbd7d19e604596a33b5f67b05b8ce8d9
Glitch (hello-webpage) <none>
2021-03-22 06:05:17 +0000

    🐬🍔 Checkpoint ./style.css:30019375/573 ./index.html:30019375/39 ./script.js:30019375/345

--- script.js
+++ script.js
@@ -3,3 +3,19 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+var pattern = [2,2,4,3,2,1,2,4]; 
+var progress = 0; 
+var gamePlaying = false; 
+
+function startGame(){
+  progress = 0;
+  gamePlaying = true; 
+  
+  document.getElementById("startBtn").classList.add("hidden"); 
+  document.getElementById("stopBtn")
+}
+
+function stopGame(){
+  gamePlaying = false; 
+}
